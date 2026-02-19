6444fad621a87383696b845f7f12dd0a3287ce26
Rowin <rowinmmathias@gmail.com>
2026-02-19 19:59:37 +0530

    yo yo

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,17 +1,19 @@
 import pino from 'pino';
 import { config } from '../config';
 
+const isCloud = process.env.VERCEL || process.env.RENDER || process.env.NETLIFY;
+const isDev = config.nodeEnv === 'development' && !isCloud;
+
 export const logger = pino({
   level: config.nodeEnv === 'development' ? 'debug' : 'info',
-  transport:
-    config.nodeEnv === 'development'
-      ? {
-          target: 'pino-pretty',
-          options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-            ignore: 'pid,hostname',
-          },
-        }
-      : undefined,
+  transport: isDev
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+          ignore: 'pid,hostname',
+        },
+      }
+    : undefined,
 });
